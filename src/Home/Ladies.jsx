ae7766dbfdf0fbc49f9ea9d9bc20b67643be9efc
maryamsaye <marynsaye@gmail.com>
2025-01-25 07:31:37 -0800

    added Partners.jsx

---
+++ src/Home/Ladies.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Ladies.css";
+import  group2 from "../assets/group2.svg";
+
+
+import React from 'react'
+
+const Ladies = () => {
+    return (
+        <div className="group2">
+            <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt impedit cumque deleniti facilis, reprehenderit eaque! Architecto aut eveniet nulla officia.</p>
+            <button className="black">Learn More</button>
+
+        </div>
+    )
+}
+
+export default Ladies
