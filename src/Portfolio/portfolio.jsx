0373893391d293a15efced1c38ca617f95f4997c
maryamsaye <marynsaye@gmail.com>
2025-02-08 07:47:50 -0800

    added portfolio.jsx

--- src/Portfolio/portfolio.jsx
+++ src/Portfolio/portfolio.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
+import './portfolio.css';
+import  landingpage from "../assets/landingpage.svg";
+import  webdesign from "../assets/webdesign.svg";
+import  rocket from "../assets/rocket.svg";
+import  shopping from "../assets/shopping.svg";
+import  geometric from "../assets/geometric.svg";
+import  tricks from "../assets/tricks.svg";
+import  halloween from "../assets/halloween.svg";
+import  virtual from "../assets/virtual.png";
 
-
-
-import Navbar from '../Components/Navbar/Navbar';
-
-
-const portfolio = () => {
+const works = () => {
   return (
-    <div>
-        <Navbar />  
+    <section className='work-container'>
+      <div className='work'>
+        <h1 className='head'>WORKS</h1>
+        <h2 className='body'>PORTFOLIO</h2>
+        <p className='lorem'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+      <div className='photos'>
+          <img src={landingpage} alt="" className="img" />
+          <img src={webdesign} alt="" className="img" />
+          <img src={rocket} alt="" className="img" />
+          <img src={shopping} alt="" className="img" />
+          <img src={geometric} alt="" className="img" />
+          <img src={tricks} alt="" className="img" />
+          <img src={halloween} alt="" className="img" />
+          <img src={virtual} alt="" className="img" />
+
+        </div>
+      
+
     </div>
+    </section>
   )
 }
 
-export default portfolio;
+export default works;
+
